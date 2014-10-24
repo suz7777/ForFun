f85782094d493698973c1e197ed67144e9af583b
suz <suzanne.george@perficient.com>
2014-10-23 21:45:15 -0500

    initial checkin

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    public class suznumbers
+    {
+        public static Tuple<int, string>[] combinations = new Tuple<int, string>[] 
+        { 
+            new Tuple<int, string> (3, "Fizz"), 
+            new Tuple<int, string> (5, "Buzz"), 
+        };
+
+        public static string check_mult(int i)
+        {
+            string ret = "";
+            foreach (var comb in combinations)
+            {
+                if (i % comb.Item1 == 0)
+                    ret += comb.Item2;
+            }
+            return ret;
+        }
+
+        public static string fizzbuzz(int i)
+        {
+            string ret = check_mult(i);
+            if (ret.Length == 0)
+                ret = i.ToString();
+            return ret;
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using ConsoleApplication1;
+
+
+namespace ConsoleApplication1
+{
+
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 50;
+            for (int i = start; i < end; i++)
+                Console.WriteLine(ConsoleApplication1.suznumbers.fizzbuzz(i));
+            Console.ReadLine();
+        }
+    }
+}
+
